65f70a9237df0f5abf587e93fd8bbaacfcfd1f84
Walter Pereira Cruz <wpereira04@vt.edu>
2025-04-03 13:21:57 -0400

    added code unsure of working

---
+++ include/db.h
@@ -0,0 +1,95 @@
+﻿////////////////////////////////////////////////////////
+//
+// ECE 3574, A6, Walter Pereira Cruz
+// File name: db.h
+// Description: Contains the declarations for the Database class
+// functions and variables
+//				
+// Date:        03/14/2025
+//
+
+#ifndef DATABASE_H
+#define DATABASE_H
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <ctime>
+
+class Database {
+private:
+    //member variable "db" is of string type
+    std::string db;
+    
+    //member variable "username" is of string type
+    std::string username;
+    //member variable "password" is of string type
+    
+    std::string password;
+    //member variable "connected" is of string type
+    //The "connected" is set to false initially.
+    
+    bool connected{ false };
+    //member variable "instance" is static 
+    //"instance" (pointer to Database) is a staic variable that stores the instance of the database. Its value is set in the function "getInstance"
+    
+    static Database* instance;
+
+    time_t last_activity{ std::time(0) };
+
+    static const int TIMEOUT{ 5 };
+
+    //constructor that helps creating instance of db(e.g. sales.db) accepts name of the database, username, password. 
+    
+    Database(const std::string& dbName, const std::string& usrname, const std::string& pasword)
+        : db(dbName), username(usrname), password(pasword) {}
+
+public:
+    //destructor that disconnects the connection if connected.
+    
+    ~Database();
+    //"getInstance" that creates and returns the instance of the database. If called first time it sets the username and password. However, subsequent time, it matches the database name, username and password and returns the previous instance if matched else it throws std::runtime_error("invalid database name, username or password"). We are using Singleton Design Pattern that creates only one instance of the databse. The instance is still created by the constructor.
+    
+    static Database* getInstance(const std::string& dbName, const std::string& usrname, const std::string& pasword);
+    //"connect" that sets "connected" to true (return void)
+    void connect();
+    //"disconnect" that sets "connected" to false (return void)
+    void disconnect();
+    // retrun status of connected true/false (return bool)
+    
+    bool isTimeout();
+    
+    void refreshConnection();
+
+    bool isConnected();
+    //overload the new operator that allocates memory using malloc of given size and returns pointer of type void and prints " overloaded new " (cout is okay in this case). std::cout << "overloaded new ";
+    //If the memory allocation fails it should throw std::bad_alloc()
+    
+    void* operator new(size_t size);
+    //overload the delete operator that deallocates memory and prints "overloaded delete " (cout is okay in this). std::cout << "overloaded delete ";
+    
+    void operator delete(void* pt);
+    //set_username and get_username for username
+    
+    
+    void set_username(const std::string& username);
+    const std::string get_username();
+    //set_password and get_password for password.
+    
+    
+    void set_password(const std::string& password);
+
+    const std::string get_password();
+    //The static "resetInstance" as defined below.
+    static void resetInstance();
+
+    void operator=(const Database& db);
+
+    Database(const Database& db);
+
+    Database(Database&& db);
+
+    void operator=(Database&& db);
+};
+
+
+#endif
---
+++ src/db.cpp
@@ -0,0 +1,128 @@
+﻿////////////////////////////////////////////////////////
+//
+// ECE 3574, A6, Walter Pereira Cruz
+// File name: db.cpp
+// Description: Contains the implementations for the Database class
+// functions
+//				
+// Date:        03/14/2025
+//
+#include "db.h"
+
+// intializes instance to nullptr
+
+Database* Database::instance = nullptr;
+
+// default destructor
+Database::~Database() {
+
+}
+
+// checks if the instance is occupied which determines if it's "the first run" or not. 
+// creates an instance and returns it or checks for matching information and either returns the instance
+// or throws a runtime error.
+Database* Database::getInstance(const std::string& dbName, const std::string& usrname, const std::string& pasword) {
+	if (!instance) {
+		instance = new Database(dbName, usrname, pasword);
+	}
+	else {
+		if (instance->db != dbName || instance->username != usrname || instance->password != pasword) {
+			throw std::runtime_error("invalid database name, username, or password");
+		}
+	}
+	return instance;
+}
+
+// sets connected to true
+void Database::connect() {
+	refreshConnection();
+	connected = true;
+}
+
+// sets connected to false
+void Database::disconnect() {
+	refreshConnection();
+	connected = false;
+}
+
+// returns the state of connected
+bool Database::isConnected() {
+	refreshConnection();
+	return connected;
+}
+
+// override the new operator with new definition
+void* Database::operator new(size_t size) {
+	std::cout << "overloaded new ";
+	void* ptr = std::malloc(size);
+	if (!ptr) {
+		throw std::bad_alloc();
+	}
+	return ptr;
+}
+
+// override the delete operator with another definition
+void Database::operator delete(void* pt) {
+	std::cout << "overloaded delete ";
+	std::free(pt);
+}
+
+// sets the username member variable
+void Database::set_username(const std::string& usrname) {
+	refreshConnection();
+	username = usrname;
+}
+
+// sets the password member variable
+void Database::set_password(const std::string& pasword) {
+	refreshConnection();
+	password = pasword;
+}
+
+// gets the username member variable
+const std::string Database::get_username() {
+	refreshConnection();
+	return username;
+}
+
+// gets the password member variable
+const std::string Database::get_password() {
+	refreshConnection();
+	return password;
+}
+
+// reset the instance
+void Database::resetInstance() {
+	if (instance != nullptr) {
+		delete instance;
+		instance = nullptr;
+	}
+}
+
+void Database::operator=(const Database& db)
+{
+	throw(std::runtime_error("Copy or Move operations are not allowed"));
+}
+
+Database::Database(const Database& db)
+{
+	throw(std::runtime_error("Copy or Move operations are not allowed"));
+}
+
+Database::Database(Database&& db)
+{
+	throw(std::runtime_error("Copy or Move operations are not allowed"));
+}
+
+void Database::operator=(Database&& db)
+{
+	throw(std::runtime_error("Copy or Move operations are not allowed"));
+}
+
+bool Database::isTimeout() {
+	return (std::difftime(last_activity, std::time(0)) >= TIMEOUT);
+}
+
+void Database::refreshConnection() {
+	last_activity = std::time(0);
+}
